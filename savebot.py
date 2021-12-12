14d8b22abb5bd2752afd627cea7145cdcade3e7f
FossaSPB <95774332+FossaSPB@users.noreply.github.com>
2021-12-12 14:37:12 +0300

    Nothing

---
+++ savebot.py
@@ -0,0 +1,21 @@
+import telebot
+import constants
+from datetime import date
+from telebot import types
+
+API_TOKEN = '2073493631:AAGMTA4EdsHWFzuM3Dh8tkliNU7OCOjIEmo'
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['ves'])
+def handle_text (message):
+    bot.send_message(message.chat.id, "сколько ты весишь? (в кило, блять)")
+    @bot.message_handler(content_types=['text'])
+    def handle_text(message):
+        txt = message.text
+        my_file = open('lohpidr.txt', 'w')
+        text_for_file = message.text
+        today = date.today()
+        d1 = today.strftime("%d/%m/%Y")
+        my_file.write(text_for_file + ' ' + d1)
+        bot.send_message(message.chat.id, 'жиробас. я тебя запомнил')
+bot.polling() 
